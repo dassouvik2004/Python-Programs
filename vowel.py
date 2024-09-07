65e62898100e1cba11677c4c1ec9c3e3ff6979c9
dassouvik2004 <souvikdas9102004@gmail.com>
2024-09-07 21:54:55 +0530

    Vowel Check

--- vowel.py
+++ vowel.py
@@ -1,9 +1,5 @@
-string1 = input("Enter any string: ")
-string1 = string1.upper()
-count = 0
-
-for char in string1:
-    if char in "AEIOU":
-        count += 1
-
-print(count)
+s = "m"
+if s=="a" or s=="e" or s=="i" or s=="o" or s=="u":
+    print(s,"is a vowel.")
+else:
+    print(s,"is not a vowel.")
